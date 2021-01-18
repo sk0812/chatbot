ecb144c84edf2231b5629e792b6e0c34ef4c5c26
Sid Kheria <sidkheria@gmail.com>
2021-01-18 14:51:14 +0000

    removed test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-for x in range(3):
-    info = input("")
-    age = [int(s) for s in info.split() if s.isdigit()]
-    age = age[0]
-    age = str(age)
-    print(age)
-    print(info.replace(age, ""))
