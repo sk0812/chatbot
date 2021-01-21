7f965f9e6ff7ba77ed22a5ab032439d151476bb2
Sid Kheria <sidkheria@gmail.com>
2021-01-21 13:30:34 +0000

    search youtube skill import

--- skill_import.py
+++ skill_import.py
@@ -16,7 +16,7 @@ from skills.search.search_ebay import search_ebay
 from skills.misc.random_number import random_number
 from skills.misc.joke import joke
 
-skills = ["current_weather", "current_weather_context", "today_weather", "today_weather_context", "tomorrow_weather", "tomorrow_weather_context","dice_roll", "coin_flip", "day", "time", "what_can_you_do", "search_google", "search_amazon", "search_ebay", "random_number", "joke", "tonight_weather", "current_temp"]
+skills = ["current_weather", "current_weather_context", "today_weather", "today_weather_context", "tomorrow_weather", "tomorrow_weather_context","dice_roll", "coin_flip", "day", "time", "what_can_you_do", "search_google", "search_amazon", "search_ebay", "search_youtube","random_number", "joke", "tonight_weather", "current_temp"]
 
 websites = ["open_youtube", "open_habsnet", "open_outlook", "open_smhw", "open_amazon", "open_ebay", "open_gmail", "open_bbc_bitesize", "open_google", "open_github", "open_google_drive", "open_imdb", "open_reddit", "open_netflix", "open_wikipedia"]
 
